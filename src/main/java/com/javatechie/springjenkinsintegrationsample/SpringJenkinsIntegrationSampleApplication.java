8a299abfb01e9f8f02ac5dee52246ee30fc63557
paveldarii <paveldarii@yahoo.com>
2024-04-05 12:29:26 -0500

    Initial commit

---
+++ src/main/java/com/javatechie/springjenkinsintegrationsample/SpringJenkinsIntegrationSampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.springjenkinsintegrationsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsIntegrationSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJenkinsIntegrationSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javatechie/springjenkinsintegrationsample/SpringJenkinsIntegrationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.springjenkinsintegrationsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsIntegrationSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
