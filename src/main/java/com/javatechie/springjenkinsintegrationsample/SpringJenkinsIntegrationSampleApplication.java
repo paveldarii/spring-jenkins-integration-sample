b185d062ff3fb37a9881a701d7e0e953d8b6ad83
paveldarii <paveldarii@yahoo.com>
2024-04-05 15:35:45 -0500

    second commit

--- src/main/java/com/javatechie/springjenkinsintegrationsample/SpringJenkinsIntegrationSampleApplication.java
+++ src/main/java/com/javatechie/springjenkinsintegrationsample/SpringJenkinsIntegrationSampleApplication.java
@@ -2,12 +2,19 @@ package com.javatechie.springjenkinsintegrationsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringJenkinsIntegrationSampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringJenkinsIntegrationSampleApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String home() {
+        return "Hello Friend";
+    }
 }
